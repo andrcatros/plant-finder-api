197247a322a27e58aa011a6516e30afb37ae43ab
Andreea Ros <ros.andreeacatalina@gmail.com>
2020-09-14 18:50:52 +0100

    Set up basic app, including MongoDB connection and test endpoint.

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
 
-var app = express();
+// configure app with env variables
+require("dotenv").config();
+const PORT = process.env.PORT || 4000;
+const mongoURL = process.env.URL;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const jsonParser = bodyParser.json();
+app.use(jsonParser);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(cors({ credentials: true, origin: true }));
+app.options("*", cors());
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// initialise database
+mongoose.connect(mongoURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+const db = mongoose.connection;
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+db.options = {};
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("You have connected to the database.");
+  app.listen(PORT, () => {
+    console.log(`Plant Finder API is running on :${PORT}`);
+  });
+
+  app.use("/test", indexRouter);
 });
 
 module.exports = app;
